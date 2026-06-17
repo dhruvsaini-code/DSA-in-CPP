b5e2ed4e544b4bb9db6c2efce01a1e4c87a44eda
dhruvsaini <dhruvsainims231@gmail.com>
2026-06-17 14:57:29 +0530

    Add Power of Two solution

---
+++ Problems/poweroftwo.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i <= 30; i++) {
+    int ans = pow(2, i);
+    if (ans == n) {
+      cout << "true" << endl;
+      return 0;
+    }
+  }
+  cout << "false" << endl;
+  return 0;
+}
