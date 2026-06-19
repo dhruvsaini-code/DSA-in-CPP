9379bfaabd3ff27ccd2426fdcbaf71f17c300f76
dhruvsaini <dhruvsainims231@gmail.com>
2026-06-19 19:19:25 +0530

    Add switch statement examples

---
+++ ControlFlow/switch.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
+int main() {
+  /*int num = 2;
+  cout << endl;
+  switch (num) {
+  case 1:
+    cout << "First" << endl;
+    break;
+  case 2:
+    cout << "Second" << endl;
+    break;
+  default:
+    cout << "Default" << endl;
+    break;
+  }*/
+  /*int num = 2;
+  char ch = '1';
+
+  switch (ch) {
+  case 1:
+    cout << "first" << endl;
+    break;
+  case '1':
+    switch (num) {
+    case 1:
+      cout << "Number is- " << num << endl;
+      break;
+    case 2:
+      cout << "Number is " << num << endl;
+      break;
+    }
+    break;
+  default:
+    cout << "Default" << endl;
+  }*/
+  /*int a, b;
+  cout << "Enter value of a : " << endl;
+  cin >> a;
+  cout << "Enter value of b : " << endl;
+  cin >> b;
+  char op;
+  cout << "Enter the function you want to perform : " << endl;
+  cin >> op;
+  switch (op) {
+  case '+':
+    cout << (a + b) << endl;
+    break;
+  case '-':
+    cout << (a - b) << endl;
+    break;
+  case '*':
+    cout << (a * b) << endl;
+    break;
+  case '/':
+    cout << (a / b) << endl;
+    break;
+  case '%':
+    cout << (a % b) << endl;
+    break;
+  default:
+    cout << "No such operator" << endl;
+    break;
+  }*/
+
+  /*int amount;
+  cout << "Enter the amount" << endl;
+  cin >> amount;
+  int Rs100, Rs50, Rs20, Rs1;
+
+  switch (1) {
+  case 1:
+    Rs100 = amount / 100;
+    amount = amount % 100;
+    cout << "No of 100 Rupee notes required are=" << Rs100 << endl;
+
+  case 2:
+    Rs50 = amount / 50;
+    amount = amount % 50;
+    cout << "No of 50 Rupee notes required are=" << Rs50 << endl;
+
+  case 3:
+    Rs20 = amount / 20;
+    amount = amount % 20;
+    cout << "No of 20 Rupee notes required are=" << Rs20 << endl;
+
+  case 4:
+    Rs1 = amount / 1;
+    amount = amount % 1;
+    cout << "No of 1 Rupee notes required are=" << Rs1 << endl;
+  }*/
+
+  
+}
